b954bc495759af7268892734c944605240172c71
fabuandras <fabuandras@gmail.com>
2025-12-12 11:53:12 +0100

    Backend: seederek letrehozva

--- database/migrations/2025_12_12_095826_create_kategoriak_table.php
+++ database/migrations/2025_12_12_095826_create_kategoriak_table.php
@@ -6,21 +6,15 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('kategoriak', function (Blueprint $table) {
             $table->id();
-            $table->string('nev');
+            $table->string('kategoria_nev')->unique();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('kategoriak');
--- database/migrations/2025_12_12_100703_create_ingatlanok_table.php
+++ database/migrations/2025_12_12_100703_create_ingatlanok_table.php
@@ -1,33 +1,31 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('ingatlanok', function (Blueprint $table) {
             $table->id();
+
             $table->foreignId('kategoria_id')
-                  ->constrained('kategoriak')
-                  ->cascadeOnDelete();
-            $table->string('cim');
-            $table->integer('ar');
-            $table->integer('terulet');
+                ->constrained('kategoriak')
+                ->cascadeOnDelete();
+
+            $table->text('leiras');
+            $table->dateTime('datum')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->boolean('tehermentes')->default(true);
+            $table->unsignedInteger('ar');
+            $table->string('kepUrl');
 
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('ingatlanok');
